897191bf225ec7c08e2dc733fd4f9e922e65ae19
Francis Maclang <josefrancisco.maclang@upr.edu>
2022-03-10 16:31:32 -0400

    installed gunicorn to allow heroku deployment

---
+++ app/main.py
@@ -0,0 +1,47 @@
+import os
+import psycopg2
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, Class Track!'
+
+@app.route('/connect-db')
+def connect():
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+            host='ec2-34-231-183-74.compute-1.amazonaws.com',
+            database='ddetn88o84e93n',
+            user='oisewifxugdivf',
+            password='155a4e05bef9407085766f6326277a143b1aa857ed8210c48a4f4517947dd563'
+        )
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        return error
+    finally:
+        if conn is not None:
+            conn.close()
+            message = 'PostgreSQL database version: '
+            for string in db_version:
+                message += str(string)
+            return  message
