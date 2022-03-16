7df2a274e6bfb7f5180af5b62828f27a09c4021e
Ignacio Tampe <Igtampe@gmail.com>
2022-03-16 01:12:12 -0400

    Add sessions with Departments (will need to be todo)

--- app/routes/departments_route.py
+++ app/routes/departments_route.py
@@ -9,6 +9,13 @@ app_departments_routes = Blueprint('departments_routes', __name__)
 # CREATE Department
 @app_departments_routes.route('/classTrack/department', methods=['POST'])
 def create_department():
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
+    if not False:  # TODO Check for admin role and/or check if this is one of the university's admin
+        return make_response("Insufficient permissions", 403)
+
     data = request.get_json()
     department_access = Departments()
     department_id = department_access.create(
@@ -37,6 +44,13 @@ def get_department(id):
 # UPDATE
 @app_departments_routes.route('/classTrack/department/update/<int:id>', methods=['PUT'])
 def update_department(id):
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
+    if not False:  # TODO Check for admin role and/or check if this is one of the university's admin
+        return make_response("Insufficient permissions", 403)
+
     data = request.get_json()
     department_access = Departments()
     if department_access.read(id) is None:
@@ -50,6 +64,13 @@ def update_department(id):
 # DELETE
 @app_departments_routes.route('/classTrack/department/delete/<int:id>', methods=['POST'])
 def delete_department(id):
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
+    if not False:  # TODO Check for admin role and/or check if this is one of the university's admin
+        return make_response("Insufficient permissions", 403)
+
     department_access = Departments()
     if department_access.read(id) is None:
         department_access.close_connection()
