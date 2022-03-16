a484a8afe63095d39bab062d545a40ce92d56b95
Ignacio Tampe <Igtampe@gmail.com>
2022-03-16 01:30:01 -0400

    add Auth to curriculums

--- app/routes/curriculums_route.py
+++ app/routes/curriculums_route.py
@@ -9,7 +9,15 @@ app_curriculum_routes = Blueprint('curriculums_routes', __name__)
 # CREATE Curriculum
 @app_curriculum_routes.route('/classTrack/curriculum', methods=['POST'])
 def create_curriculum():
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
     data = request.get_json()
+
+    if s.user_id != data["user_id"]:
+        return make_response("Cannot make a curriculum for a user that you aren't!", 403)
+
     curriculum_access = Curriculums()
     curriculum_id = curriculum_access.create(
         data["deptCode"], data["user_id"], data["department_id"])
@@ -37,6 +45,10 @@ def get_curriculum(id):
 # UPDATE
 @app_curriculum_routes.route('/classTrack/curriculum/update/<string:id>', methods=['PUT'])
 def update_curriculum(id):
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
     data = request.get_json()
     curriculum_access = Curriculums()
     updated_curriculum = curriculum_access.update(
@@ -47,7 +59,21 @@ def update_curriculum(id):
 # DELETE
 @app_curriculum_routes.route('/classTrack/curriculum/delete/<string:id>', methods=['POST'])
 def delete_curriculum(id):
+    s = SManager.get_tied_user(request.headers.get("SessionID"))
+    if s is None:
+        return make_response("Invalid Session", 401)
+
     curriculum_access = Curriculums()
+
+    c = curriculum_access.read(id)
+    if c is None:
+        curriculum_access.close_connection()
+        return make_response(jsonify({"err":"Curriculum was not found"}), 404)
+
+    if c.user_id != s.user_id:
+        curriculum_access.close_connection()
+        return make_response("Cannot delete a curriculum you do not own", 403)
+
     deleted_curriculum = curriculum_access.delete(id)
     curriculum_access.close_connection()
     return make_response(jsonify({"curriculum_id": deleted_curriculum}), 200)
