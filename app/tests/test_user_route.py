f268f0e491c55e354b06856114baa5044e74f927
Julian E. Ramos Velez <julian.ramos1@upr.edu>
2022-03-31 08:39:36 -0400

    Added @pytest.fixture's for login in 'test_user_route' and 'test_curriculum_rating_route' so that the sessionID could be accessed without hassle. All the tests that were added are like a skeleton, they can be changed, will continue with the rest of the tests once the mocks are discussed

--- app/tests/test_user_route.py
+++ app/tests/test_user_route.py
@@ -7,6 +7,10 @@ test_user = {
     "email": "test@testjulian.com",
     "password": "test"
 }
+test_admin = {
+    "email": "admin@account.com",
+    "password": "admin"
+}
 
 
 @pytest.fixture
@@ -25,6 +29,28 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture
+def login_user(client):
+    login = client.post('classTrack/login', json=test_user)
+    assert type(login.get_data()) == bytes
+
+    # Removing new lines from Session ID and decoding so it could be json compatible
+    user_sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
+
+    yield user_sessionID
+
+
+@pytest.fixture
+def login_admin(client):
+    login = client.post('classTrack/login', json=test_admin)
+    assert type(login.get_data()) == bytes
+
+    # Removing new lines from Session ID and decoding so it could be json compatible
+    admin_sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
+
+    yield admin_sessionID
 
 
 def test_create_admin(client):
@@ -67,27 +93,15 @@ def test_login_user(client):
     assert response.status_code == 200
 
 
-def test_logout_user(client):
-    # Login user
-    login = client.post('classTrack/login', json=test_user)
-    assert type(login.get_data()) == bytes
-
-    # Removing new lines from Session ID and decoding so it could be json compatible
-    sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
-
-    response = client.post('classTrack/logout', json={"session_id": sessionID})
+def test_logout_user(client, user_sessionID):
+    response = client.post('classTrack/logout',
+                           json={"session_id": user_sessionID})
     assert response.status_code == 200
 
 
-def test_get_me(client):
-    # Login user
-    login = client.post('classTrack/login', json=test_user)
-    assert type(login.get_data()) == bytes
-
-    # Removing new lines from Session ID and decoding so it could be json compatible
-    sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
-
-    response = client.post('classTrack/me', json={"session_id": sessionID})
+def test_get_me(client, user_sessionID):
+    response = client.post(
+        'classTrack/me', json={"session_id": user_sessionID})
     assert response.status_code == 200
 
 
@@ -95,10 +109,58 @@ def test_get_all_users(client):
     response = client.get('classTrack/users')
     assert response.status_code == 200
 
+
 def test_get_student_by_id(client):
     response = client.get('classTrack/student/70')
     assert response.status_code == 200
 
+
 def test_get_admin_by_id(client):
-    response = client.get('classTrack/admin/70')
+    response = client.get('classTrack/admin/61')
+    assert response.status_code == 200
+
+# Update
+
+
+def test_update_user_by_user(client, user_sessionID):
+    test_user = {
+        "isAdmin": False,
+        "variant_id": 4,
+        "first_name": "User test",
+        "last_name": "Account",
+        "email": "user@testaccount.com",
+        "password": "user"
+    }
+
+    response = client.put('classTrack/user/update/70',
+                          json=test_user, headers={'SessionID': user_sessionID})
+    assert response.status_code == 200
+
+
+def test_update_user_by_admin(client, admin_sessionID):
+    test_user = {
+        "isAdmin": False,
+        "variant_id": 4,
+        "first_name": "User test",
+        "last_name": "Account",
+        "email": "user@testaccount.com",
+        "password": "user"
+    }
+
+    response = client.put('classTrack/user/update/70',
+                          json=test_user, headers={'SessionID': admin_sessionID})
+    assert response.status_code == 200
+
+# Delete
+
+
+def test_delete_user_by_user(client, user_sessionID):
+    response = client.post('classTrack/user/delete/70',
+                           headers={"SessionID": user_sessionID})
+    assert response.status_code == 200
+
+
+def test_delete_user_by_admin(client, admin_sessionID):
+    response = client.post('classTrack/user/delete/70',
+                           headers={"SessionID": admin_sessionID})
     assert response.status_code == 200
