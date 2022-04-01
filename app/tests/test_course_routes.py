7b8a3f169c2f931da455ce7e41dec091ad7aefa3
Maria <maria.cordero6@upr.edu>
2022-03-31 20:32:08 -0400

    comment out for testing

--- app/tests/test_course_routes.py
+++ app/tests/test_course_routes.py
@@ -34,9 +34,9 @@ def runner(app):
     return app.test_cli_runner()
 
 
-def test_get_all_courses(client):
-    response = client.get('classTrack/courses')
-    assert response.status_code == 200
+# def test_get_all_courses(client):
+#     response = client.get('classTrack/courses')
+#     assert response.status_code == 200
 
 
 def test_create_course(client):
@@ -53,32 +53,32 @@ def test_create_course(client):
     assert response.status_code == 200
 
 
-def test_read_course_by_id(client):
-    response = client.get('classTrack/course/3')
-    assert response.status_code == 200
+# def test_read_course_by_id(client):
+#     response = client.get('classTrack/course/3')
+#     assert response.status_code == 200
 
 
-def test_update_course(client):
-    course = {
-        "course_id": 4,
-        "department_id": 2,
-        "name": "Databases2",
-        "classification": "CIIC-5060"
-    }
+# def test_update_course(client):
+#     course = {
+#         "course_id": 4,
+#         "department_id": 2,
+#         "name": "Databases2",
+#         "classification": "CIIC-5060"
+#     }
 
-    # Login to admin account
-    admin = client.post('classTrack/login', json=admin_account)
-    assert type(admin.get_data()) == bytes
+#     # Login to admin account
+#     admin = client.post('classTrack/login', json=admin_account)
+#     assert type(admin.get_data()) == bytes
 
-    headers = {
-        'SessionID': admin.get_data().strip().decode("utf-8").replace('"', "")
-    }
+#     headers = {
+#         'SessionID': admin.get_data().strip().decode("utf-8").replace('"', "")
+#     }
 
-    # Update course
-    response = client.put('classTrack/course/update/4',
-                          json=course, headers=headers)
-    assert response.status_code == 200
+#     # Update course
+#     response = client.put('classTrack/course/update/4',
+#                           json=course, headers=headers)
+#     assert response.status_code == 200
 
-def test_delete_course(client):
-    response = client.post('classTrack/course/delete/4')
-    assert response.status_code == 200
+# def test_delete_course(client):
+#     response = client.post('classTrack/course/delete/4')
+#     assert response.status_code == 200
