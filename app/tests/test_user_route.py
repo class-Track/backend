b3eea6bb6b4b0ca89ae3eeed461b79cfddf26384
Maria <maria.cordero6@upr.edu>
2022-04-01 16:46:51 -0400

    test_course_routes.py has been implemented completely checking that the results are correct. test_user_route is being implemented, all the admin routes have been tested and working user routes remain

--- app/tests/test_user_route.py
+++ app/tests/test_user_route.py
@@ -1,34 +1,35 @@
-# import pytest
-# from app.main import create_app
-# import psycopg2
+import json
+import pytest
+from app.main import create_app
+import psycopg2
 
-# # Global Variables
-# test_user = {
-#     "email": "test@testjulian.com",
-#     "password": "test"
-# }
-# test_admin = {
-#     "email": "admin@account.com",
-#     "password": "admin"
-# }
-
-
-# @pytest.fixture
-# def app():
-#     app = create_app()
-#     app.config["TESTING"] = True
-#     app.testing = True
-#     yield app
+# Global Variables
+test_user = {
+    "email": "test@testjulian.com",
+    "password": "test"
+}
+test_admin = {
+    "email": "admin@account.com",
+    "password": "admin"
+}
 
 
-# @pytest.fixture
-# def client(app):
-#     return app.test_client()
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+    app.testing = True
+    yield app
 
 
-# @pytest.fixture
-# def runner(app):
-#     return app.test_cli_runner()
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
 
 
 # @pytest.fixture
@@ -53,20 +54,88 @@
 #     yield admin_sessionID
 
 
-# def test_create_admin(client):
+def test_admin_routes(client):
+    # Create admin account
+    first_name = "Admin"
+    last_name = "Account"
+    email = "admin1@account.com"
+    university_id = 2
+    password = "admin"
 
-#     data = {
-#         "isAdmin": True,
-#         "variant_id": 2,
-#         "first_name": "Admin test",
-#         "last_name": "Account 1",
-#         "email": "admin1@account.com",
-#         "password": "admin"
-#     }
+    data = {
+        "isAdmin": True,
+        "variant_id": university_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "password": password
+    }
 
-#     response = client.post('classTrack/user', json=data)
-#     assert response.status_code == 200
+    response = client.post('classTrack/user', json=data)
+    admin_id = json.loads(
+        response.get_data().strip().decode("utf-8"))['user_id']
 
+    assert response.status_code == 200 and type(admin_id) == int
+
+    # Login admin account
+    login = client.post('classTrack/login', json={"email": email, "password": password})
+    assert type(login.get_data()) == bytes
+
+    sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
+
+    # Get admin by id
+    admin_account = {
+        "email": email,
+        "first_name": first_name,
+        "last_name": last_name,
+        "university_id": university_id,
+        "user_id": admin_id
+    }
+
+    response = client.get('classTrack/admin/' + str(admin_id))
+    assert response.status_code == 200 and json.loads(
+        response.get_data().strip().decode("utf-8")) == admin_account
+
+    # Get admin by session id
+    me_data = {
+        "isAdmin": True,
+        "university_id": university_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "user_id": admin_id
+    }
+    response = client.post('classTrack/me', json={'session_id': sessionID})
+    assert response.status_code == 200 and json.loads(response.get_data().strip().decode("utf-8")) == me_data
+
+    # Update admin account
+    data["first_name"] = "Julian"
+    data["last_name"] = "Ramos"
+    data['session_id'] = sessionID
+
+    response = client.put('classTrack/user/update/' + str(admin_id), json=data)
+    assert response.status_code == 200 and json.loads(
+        response.get_data().strip().decode("utf-8"))['user_id'] == admin_id
+    
+    # Delete admin account
+    response = client.post('classTrack/user/delete/' + str(admin_id), json=data)
+    assert response.status_code == 200 and json.loads(
+        response.get_data().strip().decode("utf-8"))['user_id'] == admin_id
+
+def test_get_all_users(client):
+    response = client.get('classTrack/users')
+    assert response.status_code == 200
+
+def test_logout_user(client):
+    login = client.post('classTrack/login', json=test_user)
+    assert type(login.get_data()) == bytes
+
+    # Removing new lines from Session ID and decoding so it could be json compatible
+    user_sessionID = login.get_data().strip().decode("utf-8").replace('"', "")
+
+    response = client.post('classTrack/logout',
+                           json={"session_id": user_sessionID})
+    assert response.status_code == 200 and response.get_data().strip().decode("utf-8").replace('"', "") == 'true'
 
 # def test_get_admin_id(client):
 #     response = client.get('classTrack/admin/71')
@@ -93,20 +162,12 @@
 #     assert response.status_code == 200
 
 
-# def test_logout_user(client, user_sessionID):
-#     response = client.post('classTrack/logout',
-#                            json={"session_id": user_sessionID})
-#     assert response.status_code == 200
+
 
 
 # def test_get_me(client, user_sessionID):
 #     response = client.post(
 #         'classTrack/me', json={"session_id": user_sessionID})
-#     assert response.status_code == 200
-
-
-# def test_get_all_users(client):
-#     response = client.get('classTrack/users')
 #     assert response.status_code == 200
 
 
